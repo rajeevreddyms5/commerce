bf8ebfacb2b5087393eaff63226a95fe5bdeeeff
MMEGS <rajeevreddyms5@gmail.com>
2023-07-15 16:30:06 +0530

    initial commit

--- auctions/models.py
+++ auctions/models.py
@@ -3,4 +3,26 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    pass # Do nothing for now
+
+
+
+class Listings(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField
+    category = models.CharField(max_length=64)
+    startingbid = models.IntegerField
+    link = models.URLField(blank=True)
+    watchlist = models.ForeignKey(User, blank=True)    
+    
+
+
+class Bids(models.Model):
+    price = models.IntegerField
+
+
+class Comments(models.Model):
+    comments = models.TextField
+
+
+class Activelistings(models.Model):
