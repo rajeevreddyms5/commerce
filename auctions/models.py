251fb21f1e339ec3da47fef7d77a35e24d64dcef
MMEGS <rajeevreddyms5@gmail.com>
2023-07-17 17:57:20 +0530

    added temp models

--- auctions/models.py
+++ auctions/models.py
@@ -13,14 +13,20 @@ class Listings(models.Model):
     category = models.CharField(max_length=64)
     startingbid = models.IntegerField(null=True, blank=False)
     link = models.URLField(blank=True)
-    time = models.DateTimeField(auto_now=True)
-    watchlist = models.BooleanField(default=False)    
+    time = models.DateTimeField(auto_now_add=True)
+    listedby = models.ForeignKey(User, null=True, on_delete=models.CASCADE, related_name="listed_by")
+    status = models.BooleanField(default=False)
+    wathlist = models.ManyToManyField(User, blank=True)
+    winner = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE, related_name="won") 
     
 
 
 class Bids(models.Model):
-    price = models.IntegerField(null=True, blank=False)
+    bid = models.ManyToManyField(Listings)
+    bidby = models.ForeignKey(User, null=True, on_delete=models.CASCADE, related_name="bid_by")
+    bidprice = models.IntegerField(null=True, blank=False)
 
 
 class Comments(models.Model):
+    commentsby = models.ManyToManyField(User)
     comments = models.TextField(null=True, blank=False)
